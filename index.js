89ea4c1885e68b8b758d4af8c61b77e841b4c4a2
Alexander Cobleigh <cblgh@cblgh.org>
2019-01-02 16:57:18 +0100

    rename lib.js -> index.js

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-module.exports = require('./lib.js')
+var crypto = require('crypto')
+var Readable = require('stream').Readable
+var duplexify = require('duplexify')
+var hyperswarm = require('@hyperswarm/network')
+
+function initiate (topic, opts) {
+  var net = hyperswarm()
+  // look for peers listed under this topic
+  var topicBuffer = crypto.createHash('sha256')
+    .update(topic)
+    .digest()
+  net.join(topicBuffer, opts)
+  return net
+}
+
+exports.read = function (topic, cb) {
+  var stream = duplexify()
+  var net = initiate(topic, {
+    lookup: true // find & connect to peers
+  })
+
+  net.on('connection', (socket, details) => {
+    stream.setReadable(socket)
+    // we have received everything
+    socket.on('end', cb)
+  })
+  return stream
+}
+
+exports.write = function (topic, data) {
+  var net = initiate(topic, {
+    lookup: true, // find & connect to peers
+    announce: true // optional- announce self as a connection target
+  })
+  net.on('connection', (socket, details) => {
+    var stream = data
+    // we were passed a string note, encompass the data in a stream
+    if (typeof data === 'string') {
+      stream = new Readable()
+      stream.push(data)
+      stream.push('\n')
+      stream.push(null)
+    }
+    stream.pipe(socket)
+    // signal to the remote peer that we sent all data
+    stream.on('end', function () {
+      socket.end()
+    })
+  })
+}
